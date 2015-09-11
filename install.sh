e72d00bcc3ec5c2cc866445a92e244c57c86aacf
Vincent <vmatonis@invence.co>
2015-09-11 17:20:09 -0500

    asdf

--- install.sh
+++ install.sh
@@ -5,8 +5,4 @@ sudo dnf install vim git curl vim-jedi vim-filesystem vim-plugin-powerline vim-o
 cd ~
 
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh 
-sh spf13-vim.sh
-
-echo -e "Bundle 'unite.vim'" >> .vimrc.local
-echo -e "Bundle 'vimproc.vim'" >> .vimrc.local
-echo -e "Bundle 'vimfiler.vim'" >> .vimrc.local
+sh spf13-vim.sh && echo -e "Bundle 'unite.vim'" >> .vimrc.local && echo -e "Bundle 'vimproc.vim'" >> .vimrc.local && echo -e "Bundle 'vimfiler.vim'" >> .vimrc.local
