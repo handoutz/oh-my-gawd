bfa20295ccf35484326cee0d7d72391dc8d76d5c
Vincent <vmatonis@invence.co>
2015-09-11 17:25:47 -0500

    moar changes

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo dnf install vim git curl vim-jedi vim-filesystem vim-plugin-powerline vim-omnicppcomplete tmux zsh wget
-
+MYDIR=`pwd`
 cd ~
 
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh 
-sh spf13-vim.sh && echo -e "Bundle 'unite.vim'" >> .vimrc.local && echo -e "Bundle 'vimproc.vim'" >> .vimrc.local && echo -e "Bundle 'vimfiler.vim'" >> .vimrc.local
+sh spf13-vim.sh && echo -e "Bundle 'unite.vim'" >> .vimrc.bundles && echo -e "Bundle 'vimproc.vim'" >> .vimrc.local && echo -e "Bundle 'vimfiler.vim'" >> .vimrc.local
+rm -f .zshrc
+cp $MYDIR/.zshrc .
+cp $MYDIR/.tmux.conf .
